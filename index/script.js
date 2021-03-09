48a13ded1d12e7d0a0db008b7fa3bdaf997c7598
Erin Bachmann <57983942+EriN-B@users.noreply.github.com>
2021-03-09 13:19:08 +0100

    socials

--- index/script.js
+++ index/script.js
@@ -8,3 +8,10 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+$( document ).ready(function() {
+  $('.trigger').click(function() {
+     $('.modal-wrapper').toggleClass('open');
+    $('.page-wrapper').toggleClass('blur');
+     return false;
+  });
+});
